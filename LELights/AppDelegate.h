cce32469e5b515a8f2a69dcbd8f1fb970136a833
Laura Skelton <lauraeskelton@gmail.com>
2014-08-10 12:45:23 -0400

    Initial Commit

---
+++ LELights/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LELights
+//
+//  Created by Laura Skelton on 8/10/14.
+//  Copyright (c) 2014 lauraskelton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
